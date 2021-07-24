e98a6052ea3f1bb483ab025776d6f75afd24fc8c
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2021-07-23 22:24:07 -0500

    Update Controller.lua

--- Controller.lua
+++ Controller.lua
@@ -1,1 +1,17 @@
+local component = require("component");
+ 
+farmWidth = 4;
+farmHeight = 5;
 
+local farmX;
+function init()
+	--Create the table
+	farmX = {};
+	for i = 0, farmWidth, 1 do
+		local farmY = {};
+		for t = 0, farmHeight, 1 do --In Each FarmX Slot, Put FarmY Slots.
+			farmY[t] = nil;
+		end
+		farmX[i] = farmY;
+	end
+end
